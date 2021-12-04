4f0b554c560c72e5b5a83d899648760518a284ec
Samuel Maenle <samuel@maenle.net>
2021-12-04 23:44:09 +0100

    day 4

--- 2021/day4/day4part1.py
+++ 2021/day4/day4part1.py
@@ -1,4 +1,5 @@
 import argparse
+
 def flip(binary):
     flipped = '' 
     for i in binary:
@@ -8,40 +9,89 @@ def flip(binary):
             flipped = f'{flipped}1'
     return flipped
 
-def main(args):
-    position = 0
-    depth = 0
-    aim = 0
-    numberoflines = 0
-    dioagnosticreport = readFile(args.input)
-    lengthoflines = len(dioagnosticreport[0])
-    numberoflines = len(dioagnosticreport)
-    gammabinary = ''
-    h = 0
-    while h < lengthoflines:
-        temparray = []
-        for i in dioagnosticreport:
-            temparray.append(int(i[h]))
-        if(sum(temparray) < numberoflines/2):
-            gammabinary = f'{gammabinary}0'
-        else:
-            gammabinary = f'{gammabinary}1'   
-        h = h + 1
-
-    print(int(gammabinary, 2))
-    print(int(flip(gammabinary), 2))
-    print(int(gammabinary, 2) * int(flip(gammabinary), 2))
-   
-
 def readFile(fileName):
         fileObj = open(fileName, "r") #opens the file in read mode
         words = fileObj.read().splitlines() #puts the file into an array
         fileObj.close()
         return words
 
+def buildboards(boardsinput):
+    boards = []
+    board = []
+    for i in boardsinput:
+        if(i == ''):
+            boards.append(board)
+            board = []
+        else:
+            i = i.replace('  ', ' ').strip()
+            board.append(i.split(' '))
+    boards.append(board)
+    boards.pop(0)
+    return boards
+
+def findbingo(boards, drawnnumbers):
+    for i in drawnnumbers:
+        print(i)
+        for k in range(len(boards)):
+            for l in range(len(boards[k])): 
+                for m in range(len(boards[k][l])):    
+                    if(int(i) == int(boards[k][l][m])):
+                        boards[k][l][m] = -1  
+        for k in range(len(boards)):
+            for l in range(len(boards[k])): 
+                testsum = 0
+                for m in range(len(boards[k][l])):    
+                    testsum = testsum + int(boards[k][l][m])
+                if(testsum == -5):
+                    print(f'bingo in board {k} row {l}')
+                    ret = ['row', k, l, i]
+                    return ret
+
+        for k in range(len(boards)):
+            for l in range(len(boards[k])): 
+                testsum = 0
+                for m in range(len(boards[k][l])):    
+                    testsum = testsum + int(boards[k][m][l])
+                if(testsum == -5):
+                    print(f'bingo in board {k} column {l}')
+                    ret = ['col', k, l, i]
+                    return ret
+                 
+        print(boards)
+
+def main(args):
+    position = 0
+    depth = 0
+    aim = 0
+    numberoflines = 0
+    bingosheet = readFile(args.input)
+    drawnnumbers = bingosheet.pop(0).split(',')
+    boards = buildboards(bingosheet)
+    print(boards)
+    bingo = findbingo(boards, drawnnumbers)
+    
+    
+    winningboard = boards[bingo[1]]
+    print(winningboard)
+
+    winningsum = 0
+    for k in range(len(winningboard)):
+            for l in range(len(winningboard[k])):
+                if(int(winningboard[k][l]) != -1):
+                    winningsum = winningsum + int(winningboard[k][l])
+
+    print(f'winningsum {winningsum}')
+    print(f'winningboard {bingo[3]}')
+    print(f'result {winningsum*int(bingo[3])}')
+    
+
+    
+    
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='AdventOfCode2021Day3')
+        description='AdventOfCode2021Day4')
     parser.add_argument('--input', type=str, required=True,
                     help="File")
     args = parser.parse_args()
--- 2021/day4/day4part2.py
+++ 2021/day4/day4part2.py
@@ -1,4 +1,5 @@
 import argparse
+
 def flip(binary):
     flipped = '' 
     for i in binary:
@@ -8,51 +9,103 @@ def flip(binary):
             flipped = f'{flipped}1'
     return flipped
 
-def scrub(array, bit, lengthoflines):
-    scrubbing = array
-    h = 0
-    while h < lengthoflines:
-        numberoflines = len(scrubbing)
-        temparray = []
-        bittotest = 0
-        for i in scrubbing:
-            temparray.append(int(i[h]))
-
-        if(sum(temparray) < numberoflines/2):
-            bittotest = 0
-        else:
-            bittotest = 1      
-        if(bit == 1):
-            scrubbing = [num for num in scrubbing if int(num[h]) == int(bittotest) ]       
-        elif(bit == 0):
-            scrubbing = [num for num in scrubbing if int(num[h]) != int(bittotest) ]  
-        print(scrubbing)
-        if(len(scrubbing) == 1):
-            return scrubbing[0]
-            
-        h = h + 1
-
-
-def main(args):
-    dioagnosticreport = readFile(args.input)
-    lengthoflines = len(dioagnosticreport[0])
-
-    o2 = scrub(dioagnosticreport, 1, lengthoflines)
-    co2 = scrub(dioagnosticreport, 0, lengthoflines)    
-
-    print(int(o2, 2))
-    print(int(co2, 2))
-    print(int(o2, 2) * int(co2, 2))
-
 def readFile(fileName):
         fileObj = open(fileName, "r") #opens the file in read mode
         words = fileObj.read().splitlines() #puts the file into an array
         fileObj.close()
         return words
 
+def buildboards(boardsinput):
+    boards = []
+    board = []
+    for i in boardsinput:
+        if(i == ''):
+            boards.append(board)
+            board = []
+        else:
+            i = i.replace('  ', ' ').strip()
+            board.append(i.split(' '))
+    boards.append(board)
+    boards.pop(0)
+    return boards
+
+def findbingo(boards, drawnnumbers):
+    winningboards = []
+    wonboards = []
+    numberofboards = len(boards)
+
+    for i in drawnnumbers:
+        for k in range(len(boards)):
+            for l in range(len(boards[k])): 
+                for m in range(len(boards[k][l])):    
+                    if(int(i) == int(boards[k][l][m])):
+                        boards[k][l][m] = -1 
+
+        for k in range(len(boards) - 1, -1, -1):
+            for l in range(len(boards[k]) - 1, -1, -1):
+
+                testsum = 0
+                for m in range(len(boards[k][l]) - 1, -1, -1):   
+                    testsum = testsum + int(boards[k][l][m])
+                if(testsum == -5):
+                    print(f'bingo in board {k} row {l} winning number {i}')
+                    if(boards[k] not in wonboards):
+                        winningboards.append([boards[k], l, i])
+                        wonboards.append(boards[k])
+                        del boards[k]    
+                        print(boards)  
+
+                    if(len(wonboards) == numberofboards):
+                        return winningboards
+
+                if(len(boards) <= k):
+                    break
+
+                testsum = 0
+                for m in range(len(boards[k][l]) - 1, -1, -1):   
+                    testsum = testsum + int(boards[k][m][l])
+                if(testsum == -5):
+                    print(f'bingo in board {k} column {l} winning number {i}')
+                    if(boards[k] not in wonboards):
+                        winningboards.append([boards[k], l, i])
+                        wonboards.append(boards[k])
+                        del boards[k]
+                        print(boards) 
+
+                    if(len(wonboards) == numberofboards):
+                        return winningboards
+                
+                if(len(boards) <= k):
+                    break
+             
+
+def main(args):
+    position = 0
+    depth = 0
+    aim = 0
+    numberoflines = 0
+    bingosheet = readFile(args.input)
+    drawnnumbers = bingosheet.pop(0).split(',')
+    boards = buildboards(bingosheet)
+    print(boards)
+    bingo = findbingo(boards, drawnnumbers)
+     
+    winningboard = bingo[-1][0]
+
+    winningsum = 0
+    for k in range(len(winningboard)):
+            for l in range(len(winningboard[k])):
+                if(int(winningboard[k][l]) != -1):
+                    winningsum = winningsum + int(winningboard[k][l])
+
+    print(f'winningsum {winningsum}')
+    print(f'winningnumber {bingo[-1][2]}')
+    print(f'result {winningsum*int(bingo[-1][2])}')
+    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='AdventOfCode2021Day3')
+        description='AdventOfCode2021Day4')
     parser.add_argument('--input', type=str, required=True,
                     help="File")
     args = parser.parse_args()
